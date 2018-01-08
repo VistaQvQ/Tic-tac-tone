26f51fc8796f7e4b34447b04cf3f757e20bb4ac8
Zark Zhao <kai-memory@qq.com>
2018-01-08 14:49:08 +0800

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新游戏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.A1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            resources.ApplyResources(this.文件ToolStripMenuItem, "文件ToolStripMenuItem");
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新游戏ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            // 
+            // 新游戏ToolStripMenuItem
+            // 
+            resources.ApplyResources(this.新游戏ToolStripMenuItem, "新游戏ToolStripMenuItem");
+            this.新游戏ToolStripMenuItem.Name = "新游戏ToolStripMenuItem";
+            this.新游戏ToolStripMenuItem.Click += new System.EventHandler(this.新游戏ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            resources.ApplyResources(this.退出ToolStripMenuItem, "退出ToolStripMenuItem");
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            resources.ApplyResources(this.帮助ToolStripMenuItem, "帮助ToolStripMenuItem");
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            resources.ApplyResources(this.关于ToolStripMenuItem, "关于ToolStripMenuItem");
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // A2
+            // 
+            resources.ApplyResources(this.A2, "A2");
+            this.A2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A2.Name = "A2";
+            this.A2.Tag = "2";
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A3
+            // 
+            resources.ApplyResources(this.A3, "A3");
+            this.A3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A3.Name = "A3";
+            this.A3.Tag = "3";
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // B1
+            // 
+            resources.ApplyResources(this.B1, "B1");
+            this.B1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B1.Name = "B1";
+            this.B1.Tag = "4";
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // B2
+            // 
+            resources.ApplyResources(this.B2, "B2");
+            this.B2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B2.Name = "B2";
+            this.B2.Tag = "5";
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // B3
+            // 
+            resources.ApplyResources(this.B3, "B3");
+            this.B3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B3.Name = "B3";
+            this.B3.Tag = "6";
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // C1
+            // 
+            resources.ApplyResources(this.C1, "C1");
+            this.C1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C1.Name = "C1";
+            this.C1.Tag = "7";
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // C2
+            // 
+            resources.ApplyResources(this.C2, "C2");
+            this.C2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C2.Name = "C2";
+            this.C2.Tag = "8";
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // C3
+            // 
+            resources.ApplyResources(this.C3, "C3");
+            this.C3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C3.Name = "C3";
+            this.C3.Tag = "9";
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A1
+            // 
+            resources.ApplyResources(this.A1, "A1");
+            this.A1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A1.Name = "A1";
+            this.A1.Tag = "1";
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新游戏ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button A1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        bool turn = true;//true= x turn,false=y turn;
+        int turn_count = 0;
+        int TestNum = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Vista © 2017", "关于");
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+       
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Image == null)
+            {
+                if (turn)
+                {
+                    b.Image = WindowsFormsApplication1.Resource1.dog;
+                    b.Text = " ";
+                }
+
+                else
+                {
+                    b.Image = WindowsFormsApplication1.Resource1.cat;
+                    b.Text = "  ";
+                }
+
+                turn = !turn;
+                turn_count++;
+                checkForWiner ( );
+            }
+            else
+            {
+                MessageBox.Show ( "亲，您不可以选择这里！");
+            }
+        }
+
+        private void checkForWiner()
+        {
+            bool there_is_a_winer = false;
+            //horizontal checks
+            if ((A1.Text == A2.Text )&& (A2.Text == A3.Text)&&(A1.Image!=null))
+                there_is_a_winer = true;
+            else if ((B1.Text == B2.Text )&& (B2.Text == B3.Text) && (B1.Image != null)) 
+                there_is_a_winer = true;
+            else if ((C1.Text == C2.Text )&&( C2.Text == C3.Text) && (C1.Image != null))
+                there_is_a_winer = true;
+
+
+            //vertical checks
+            else if ((A1.Text == B1.Text) && (B1 .Text == C1.Text) && (A1.Image != null))
+                there_is_a_winer = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (A2.Image != null))
+                there_is_a_winer = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (A3.Image != null))
+                there_is_a_winer = true;
+
+
+            //diagonal checks
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (A1.Image != null))
+                there_is_a_winer = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (C1.Image != null))
+                there_is_a_winer = true;
+            if (there_is_a_winer )
+            {
+                disableButtons();
+                string winer="";
+                if (turn)
+                    winer = "Cate";
+                else
+                    winer = "Doge";
+                MessageBox.Show(winer + "   Win!!!", "Yay！");
+            }//END IF
+            else
+            {
+                if(turn_count ==9)
+                    MessageBox.Show("Draw!", "Bummer!");
+            }
+            TestNum = 100;
+        }//END checkForWiner
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }//end foreach
+            catch { }
+        }
+
+        private void 新游戏ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Image = null;
+                    b.Text = "";
+                    b.Enabled = true;
+                }
+            }//end foreach
+            catch { }
+        }
+    }
+}
